81a427fd911b5ff89e9e3ad2ab279d87034ecd17
thelinh299@gmail.com <thelinh299@gmail.com@0ad7b367-5616-68b4-1240-20a5067a2460>
2012-10-19 09:08:26 +0000

    Thay đổi màu sắc titlebar và button

--- trunk/Sources/NearFood/gen/com/nikmesoft/android/nearfood/R.java
+++ trunk/Sources/NearFood/gen/com/nikmesoft/android/nearfood/R.java
@@ -34,7 +34,7 @@ public final class R {
         public static final int titlebar_height=0x7f050000;
     }
     public static final class drawable {
-        public static final int button_background=0x7f020000;
+        public static final int button_bg=0x7f020000;
         public static final int ic_action_search=0x7f020001;
         public static final int ic_launcher=0x7f020002;
         public static final int ic_tab_checkin=0x7f020003;
@@ -49,7 +49,8 @@ public final class R {
         public static final int titlebar_back=0x7f02000c;
         public static final int titlebar_back_default=0x7f02000d;
         public static final int titlebar_back_pressed=0x7f02000e;
-        public static final int titlebar_logo=0x7f02000f;
+        public static final int titlebar_bg=0x7f02000f;
+        public static final int titlebar_logo=0x7f020010;
     }
     public static final class id {
         public static final int bottom_bar=0x7f060006;
