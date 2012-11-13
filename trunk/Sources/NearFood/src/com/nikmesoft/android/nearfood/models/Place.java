973b725e4e9e5a67fe0d07fd749ac14ef572f20d
dcm.it.bkdn@gmail.com <dcm.it.bkdn@gmail.com@0ad7b367-5616-68b4-1240-20a5067a2460>
2012-11-13 11:26:14 +0000

    Edit Place Class

--- trunk/Sources/NearFood/src/com/nikmesoft/android/nearfood/models/Place.java
+++ trunk/Sources/NearFood/src/com/nikmesoft/android/nearfood/models/Place.java
@@ -7,121 +7,68 @@ import com.google.android.maps.GeoPoint;
 @SuppressWarnings("serial")
 public class Place implements Serializable {
 
-	private long id;
+	private int id;
 	private String name;
 	private String address;
-	private String description;
 	private int likedCount;
-	private GeoPoint mapPoint;
 	private String imagePath;
-	private String referenceKey;
-	private String phoneNumber;
+	private String distance;
 
 	
 
 	public Place() {
 	}
-
-	public Place(String name, String referenceKey) {
-		this.name = name;
-		this.referenceKey = referenceKey;
-	}
-
-	public Place(String name, String address, String description, int likedCount) {
+	public Place(String name, String address, int likedCount) {
 		this.address = address;
 		this.name = name;
-		this.description = description;
 		this.likedCount = likedCount;
 	}
 
-	public Place(long id, String name, String address, String description,
-			int likedCount, GeoPoint mapPoint, String imagePath) {
+	public Place(int id, String name, String address,
+			int likedCount, String imagePath, String distance) {
 		this.id = id;
 		this.address = address;
 		this.name = name;
-		this.description = description;
 		this.likedCount = likedCount;
-		this.mapPoint = mapPoint;
 		this.imagePath = imagePath;
+		this.distance = distance;
 	}
-
-	public long getId() {
+	public int getId() {
 		return id;
 	}
-
-	public Place setId(long id) {
+	public void setId(int id) {
 		this.id = id;
-		return this;
 	}
-
 	public String getName() {
 		return name;
 	}
-
-	public Place setNamePlace(String name) {
+	public void setName(String name) {
 		this.name = name;
-		return this;
 	}
-
 	public String getAddress() {
 		return address;
 	}
-
-	public Place setAddress(String address) {
+	public void setAddress(String address) {
 		this.address = address;
-		return this;
 	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public Place setDescription(String description) {
-		this.description = description;
-		return this;
-	}
-
 	public int getLikedCount() {
 		return likedCount;
 	}
-
-	public Place setLikedCount(int likedCount) {
+	public void setLikedCount(int likedCount) {
 		this.likedCount = likedCount;
-		return this;
 	}
-
-	public GeoPoint getMapPoint() {
-		return mapPoint;
-	}
-
-	public Place setMapPoint(GeoPoint mapPoint) {
-		this.mapPoint = mapPoint;
-		return this;
-	}
-
 	public String getImagePath() {
 		return imagePath;
 	}
-
-	public Place setImagePath(String imagePath) {
+	public void setImagePath(String imagePath) {
 		this.imagePath = imagePath;
-		return this;
+	}
+	public String getDistance() {
+		return distance;
+	}
+	public void setDistance(String distance) {
+		this.distance = distance;
 	}
 
-	public String getReferenceKey() {
-		return referenceKey;
-	}
 
-	public Place setReferenceKey(String referenceKey) {
-		this.referenceKey = referenceKey;
-		return this;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public Place setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-		return this;
-	}
 }
