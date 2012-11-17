3c186ad203883460c9b0aef160bcdfbe088f21b7
dxl101bkdn@gmail.com <dxl101bkdn@gmail.com@0ad7b367-5616-68b4-1240-20a5067a2460>
2012-11-17 05:47:08 +0000

    ChangePasswordActivity.java

---
+++ trunk/Sources/NearFood/src/com/nikmesoft/android/nearfood/activities/ChangePasswordActivity.java
@@ -0,0 +1,17 @@
+package com.nikmesoft.android.nearfood.activities;
+
+import com.nikmesoft.android.nearfood.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChangePasswordActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_change_password);
+	}
+
+}
