bd8bc7f5d035b4fc27f1de10738cdb20b459e5b1
dxl101bkdn@gmail.com <dxl101bkdn@gmail.com@0ad7b367-5616-68b4-1240-20a5067a2460>
2012-11-19 10:56:59 +0000

    ChangePasswordHandler.java

---
+++ trunk/Sources/NearFood/src/com/nikmesoft/android/nearfood/handlers/ChangePasswordHandler.java
@@ -0,0 +1,23 @@
+package com.nikmesoft.android.nearfood.handlers;
+
+public class ChangePasswordHandler extends BaseHandler{
+
+	String result;
+	@Override
+	protected void processNotEndErrorCode(String localName) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void processNotStartErrorCode(String localName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected Object returnNotErrorCode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
