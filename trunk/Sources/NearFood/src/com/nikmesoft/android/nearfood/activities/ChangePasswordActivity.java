f3da61d933b320d67c2af749a37cff303665b57c
dxl101bkdn@gmail.com <dxl101bkdn@gmail.com@0ad7b367-5616-68b4-1240-20a5067a2460>
2012-11-19 10:55:57 +0000

    ChangePasswordActivity.java

--- trunk/Sources/NearFood/src/com/nikmesoft/android/nearfood/activities/ChangePasswordActivity.java
+++ trunk/Sources/NearFood/src/com/nikmesoft/android/nearfood/activities/ChangePasswordActivity.java
@@ -1,8 +1,22 @@
 package com.nikmesoft.android.nearfood.activities;
 
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
 import com.nikmesoft.android.nearfood.MyApplication;
 import com.nikmesoft.android.nearfood.R;
+import com.nikmesoft.android.nearfood.forgotpassword.GetLoveHandler;
+import com.nikmesoft.android.nearfood.handlers.ChangePasswordHandler;
+import com.nikmesoft.android.nearfood.handlers.ErrorCode;
 import com.nikmesoft.android.nearfood.utils.CommonUtil;
 import com.nikmesoft.android.nearfood.utils.Utilities;
 import android.app.Activity;
@@ -10,11 +24,12 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.AsyncTask.Status;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class ChangePasswordActivity extends Activity {
+public class ChangePasswordActivity extends BaseActivity {
 
 	private EditText new_pass,old_pass,conf_pass;
 	private ProgressDialog dialog;
@@ -35,11 +50,11 @@ public class ChangePasswordActivity extends Activity {
 		dialog.setMessage("Loading...please wait !");
 	}
 	public void onClickSave(View v){
-		if(new_pass.getText().toString().length()==0 && old_pass.getText().toString().length()==0 && conf_pass.getText().toString().length()==0)
-			Toast.makeText(getApplicationContext(), "Please enter information!", Toast.LENGTH_LONG).show();
+		if(new_pass.getText().toString().length()==0 || old_pass.getText().toString().length()==0 || conf_pass.getText().toString().length()==0)
+			Toast.makeText(getApplicationContext(), "Please fill all fields!", Toast.LENGTH_LONG).show();
 		else 
 		   if(!MyApplication.USER_CURRENT.getPassword().equals(CommonUtil.convertToMD5(old_pass.getText().toString())))
-			   Toast.makeText(getApplicationContext(), "Please enter old password!", Toast.LENGTH_LONG).show();
+			   Toast.makeText(getApplicationContext(), "Wrong password old!", Toast.LENGTH_LONG).show();
 		   else
 			   if(!conf_pass.getText().toString().equals(new_pass.getText().toString()))
 				   Toast.makeText(getApplicationContext(), "Please check confirm password!", Toast.LENGTH_LONG).show();
@@ -47,7 +62,7 @@ public class ChangePasswordActivity extends Activity {
 				   if (loader == null || loader.isCancelled()
 							|| loader.getStatus() == Status.FINISHED) {
 						loader = new WScall();
-						loader.execute(new_pass.getText().toString().trim());
+						loader.execute(String.valueOf(MyApplication.USER_CURRENT.getId()),CommonUtil.convertToMD5(new_pass.getText().toString().trim()));
 			   		}
 	}
 	public void onClickCancel(View v){
@@ -57,40 +72,82 @@ public class ChangePasswordActivity extends Activity {
 
 		@Override
 		protected Object doInBackground(String... params) {
-			// TODO Auto-generated method stub
-			return null;
+			String body="<soapenv:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">"+
+					    "<soapenv:Header/>"+
+					    "<soapenv:Body>"+
+					    "<changePassword soapenv:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">"+
+					    "<ChangePasswordRequest xsi:type=\"sfo:ChangePasswordRequest\" xmlns:sfo=\"http://nikmesoft.com/apis/SFoodServices/\">"+
+					    "<!--You may enter the following 2 items in any order-->"+
+					    "<id xsi:type=\"xsd:int\">"+Integer.parseInt(params[0])+"</id>"+
+					    "<newpassword xsi:type=\"xsd:string\">"+params[1]+"</newpassword>"+
+					    "</ChangePasswordRequest>"+
+					    "</changePassword>"+
+					    "</soapenv:Body>"+
+					    "</soapenv:Envelope>";
+			return xmlParser(Utilities.callWS(body, "http://nikmesoft.com/apis/SFoodServices/index.php/changePassword"));
 		}
 
 		@Override
 		protected void onCancelled() {
-			// TODO Auto-generated method stub
 			super.onCancelled();
 		}
 
 		@Override
-		protected void onCancelled(Object result) {
-			// TODO Auto-generated method stub
-			super.onCancelled(result);
-		}
-
-		@Override
 		protected void onPostExecute(Object result) {
-			// TODO Auto-generated method stub
 			super.onPostExecute(result);
+			try{
+				if(result.toString().equals("")){
+					Toast.makeText(getApplicationContext(), "Successfuly !", Toast.LENGTH_LONG).show();
+					setResult(RESULT_CANCELED);
+					dialog.dismiss();
+				}
+				else{
+					Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
+					setResult(RESULT_CANCELED);
+					dialog.dismiss();
+				    }
+				}
+				catch(Exception e)
+				{
+					
+				}
 		}
 
 		@Override
 		protected void onPreExecute() {
-			// TODO Auto-generated method stub
 			super.onPreExecute();
-			dialog.show();
+			
+			dialog.show();Log.d("JHJJHJHJHJ", "HJGHGHHGHGGGGGGGGGGG");
 		}
 
 		@Override
 		protected void onProgressUpdate(Integer... values) {
-			// TODO Auto-generated method stub
 			super.onProgressUpdate(values);
 		}
 		
+	}
+	private Object xmlParser(String strXml) {
+		Log.d("LLLLLLLLLLLLL", "LLLLLLLLLLLLLLLLLLLLLLLLLGGGGGGGGGGGGGGGGGGGGGGGG");
+		byte xmlBytes[] = strXml.getBytes();
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(
+				xmlBytes);
+		SAXParserFactory saxPF = SAXParserFactory.newInstance();
+		SAXParser saxParser;
+		try {
+			saxParser = saxPF.newSAXParser();
+			XMLReader xr = saxParser.getXMLReader();
+			ChangePasswordHandler handler = new ChangePasswordHandler();
+			xr.setContentHandler(handler);
+			xr.parse(new InputSource(byteArrayInputStream));
+			return handler.getResult();
+
+		} catch (ParserConfigurationException ex) {
+			ex.printStackTrace();
+		} catch (SAXException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return null;
 	}
 }
