52b1e9ae0b02e6f02fbc4b8b786885902ce265e1
dxl101bkdn@gmail.com <dxl101bkdn@gmail.com@0ad7b367-5616-68b4-1240-20a5067a2460>
2012-12-07 12:29:49 +0000

    Favorites Main

--- trunk/Sources/NearFood/src/com/nikmesoft/android/nearfood/activities/FavoritesTabGroupActivity.java
+++ trunk/Sources/NearFood/src/com/nikmesoft/android/nearfood/activities/FavoritesTabGroupActivity.java
@@ -1,8 +1,14 @@
 package com.nikmesoft.android.nearfood.activities;
 
+import com.nikmesoft.android.nearfood.MyApplication;
+
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 
 public class FavoritesTabGroupActivity extends TabGroupActivity {
 
@@ -10,6 +16,7 @@ public class FavoritesTabGroupActivity extends TabGroupActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		Log.d("dadadsdasdsdasddasd", "dad");
 		startNewActivity(FavoritesActivity.class.getSimpleName(), new Intent(this, FavoritesActivity.class));
 	}
 
